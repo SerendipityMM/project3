100e208691f46216b318b4d0fd1e0c3417ace1be
Milena <milena.mihneva@gmail.com>
2020-06-01 16:21:40 -0600

    heroku

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const expressValidator = require('express-validator');
+const path = require('path');
 
 require ('dotenv').config();
 //import routes
@@ -40,6 +41,17 @@ app.use('/api',categoryRoutes);
 app.use('/api',productRoutes);
 
 const port = process.env.PORT || 8000;
+
+
+// adding info for heroku
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('ecommerce-front/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'ecommerce-front',  'build', 'index.html')); //relative path
+
+    });
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port:  ${port}`);
